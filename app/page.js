580e0e7ae56041ec0cf16ee3b9da3556b30a2afc
golden-on <aminedahbi2016@hotmail.com>
2023-10-07 00:50:56 +0100

    banner script ads

--- app/page.js
+++ app/page.js
@@ -3,34 +3,18 @@ import styles from '../styles/Home.module.css';
 import React from 'react';
 import Layout from '../components/Layout';
 import AdBanner from '@/components/AdBanner';
-import Script from 'next/script';
 
 const IndexPage = () => {
-  const atOptions = {
-    key: 'fdc056ec88a9ae30c394fd62e5a2f255',
-    format: 'iframe',
-    height: 600,
-    width: 160,
-    params: {}
-  };
-
-  const scriptSrc = `//www.profitablecreativeformat.com/fdc056ec88a9ae30c394fd62e5a2f255/invoke.js?key=${atOptions.key}&format=${atOptions.format}&height=${atOptions.height}&width=${atOptions.width}`;
   return (
-    <>
-      <Layout>
-        <div className={styles.leftSide}>
-          <AdBanner direction="left" />
-        </div>
-        <ThumbnailDownloader className={styles.container}/>
-        <div className={styles.rightSide}>
-          <AdBanner direction="right" />
-        </div>
-      </Layout>
-      <Script
-        src={scriptSrc}
-        strategy="afterInteractive"
-      />
-    </>
+    <Layout>
+      <div className={styles.leftSide}>
+        <AdBanner direction="left" />
+      </div>
+      <ThumbnailDownloader className={styles.container}/>
+      <div className={styles.rightSide}>
+        <AdBanner direction="right" />
+      </div>
+    </Layout>
   );
 };
 
--- components/AdBanner.js
+++ components/AdBanner.js
@@ -1,12 +1,33 @@
-import Image from 'next/image';
+"use client"
 import React from 'react';
+import { useEffect, useRef } from 'react'
 
 const AdBanner = () => {
-  return (
-    <div className="ad-banner">
-      <Image width={250} height={200} src='https://www.5banners.com/store/img/cms/160x600N.gif' alt='banner' />
-    </div>
-  );
-};
+    const banner = useRef()
 
+    const atOptions = {
+        key: 'fdc056ec88a9ae30c394fd62e5a2f255',
+        format: 'iframe',
+        height: 600,
+        width: 160,
+        params: {}
+    }
+    useEffect(() => {
+        if (!banner.current.firstChild) {
+          const conf = document.createElement("script");
+          const script = document.createElement("script");
+          script.type = "text/javascript";
+          script.src = `//www.profitabledisplayformat.com/${atOptions.key}/invoke.js`;
+          conf.innerHTML = `atOptions = ${JSON.stringify(atOptions)}`;
+
+          if (banner.current) {
+            banner.current.append(conf);
+            banner.current.append(script);
+          }
+        }
+      }, []);
+  
+
+  return <div ref={banner} />;
+}
 export default AdBanner;
