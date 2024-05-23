5ebb1df478e2b7661eb8dab7aa5b1177f79b870c
golden-on <70214821+golden-on@users.noreply.github.com>
2024-05-24 00:21:50 +0100

    add Speed Insights

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,4 @@
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import { Inter } from 'next/font/google'
 import Script from 'next/script'
 import './globals.css'
@@ -15,7 +16,10 @@ export const metadata = {
  function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <SpeedInsights />
+      </body>
       <Script
         id="adsbygoogle-init"
         strategy="afterInteractive"
