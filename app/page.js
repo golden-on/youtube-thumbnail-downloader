ce8627ad8b2d8c7f2da3a2ca888789ed7bb550b8
golden-on <70214821+golden-on@users.noreply.github.com>
2024-05-24 01:14:03 +0100

    add adestra

--- app/page.js
+++ app/page.js
@@ -1,20 +1,19 @@
-import ThumbnailDownloader from '../components/ThumbnailDownloader';
-import styles from '../styles/Home.module.css';
-import React from 'react';
-import Layout from '../components/Layout';
 import AdBanner from '@/components/AdBanner';
 import AtgScript from '@/components/adCash';
+import Layout from '../components/Layout';
+import ThumbnailDownloader from '../components/ThumbnailDownloader';
+import styles from '../styles/Home.module.css';
 
 const IndexPage = () => {
   return (
     <>
       <Layout>
         <div className={styles.leftSide}>
-          <AdBanner direction="left" keys="bd9ab2950991019c8cb06f82edc31734" />
+          <AdBanner direction="left" keys="c7668cd160769b6c8115e340a7bfe179" />
         </div>
         <ThumbnailDownloader className={styles.container}/>
         <div className={styles.rightSide}>
-          <AdBanner direction="right" keys="bd9ab2950991019c8cb06f82edc31734"/>
+          <AdBanner direction="right" keys="c7668cd160769b6c8115e340a7bfe179"/>
         </div>
       </Layout>
       <AtgScript/>
--- components/AdBanner.js
+++ components/AdBanner.js
@@ -1,6 +1,5 @@
 "use client"
-import React from 'react';
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef } from 'react';
 
 const AdBanner = ({direction, keys}) => {
     const banner = useRef()
@@ -9,7 +8,7 @@ const AdBanner = ({direction, keys}) => {
         key: keys,
         format: 'iframe',
         height: 600,
-        width: 100,
+        width: 160,
         params: {}
     }
     useEffect(() => {
@@ -18,7 +17,7 @@ const AdBanner = ({direction, keys}) => {
           const conf = document.createElement("script");
           const script = document.createElement("script");
           script.type = "text/javascript";
-          script.src = `//www.profitabledisplayformat.com/${keys}/invoke.js`;
+          script.src = `//www.topcreativeformat.com/${keys}/invoke.js`;
           conf.innerHTML = `atOptions = ${JSON.stringify(atOptions)}`;
 
           if (banner.current) {
